73fc07d4739cfc31c1997ce82fd7cfb642da0bfc
Michael Manley <Michael Manley>
2016-06-17 12:33:12 -0400

    Added and converted the c# scorecard class. The model is now up to date with the c# version

--- app/src/main/java/com/example/michael/discgolfapp/ScoreCard.java
+++ app/src/main/java/com/example/michael/discgolfapp/ScoreCard.java
@@ -5,23 +5,49 @@ package com.example.michael.discgolfapp;
  */
 public class ScoreCard extends DiscGolfGameManager {
 
-    private String date;
-    private Course currentCourse;
+    private Player[] players;
+    private static int currentHole;
+    private Course course;
 
-    private Player[] players_with_Scores;
 
 
-    /*
-    public ScoreCard(Course currentCourse, Player[] playersInfo, String date){
-        this.currentCourse = currentCourse;
-        this.playersInfo = playersInfo;
-        this.date = date;
-
+    public ScoreCard(Player[] players, Course course)
+    {
+        this.players = players;
+        this.course = course;
+        currentHole = 1;
     }
-*/
+  
 
-    private void setUpScoreCard(){
+    public int getCurrentHole(){
+        return currentHole;
+    }
 
+    public Player[] getPlayers(){
+        return players;
+    }
+    public void setPlayers(Player[] value){
+        players = value;
+    }
+
+    public int getNumberOfPlayers(){
+        return players.length;
+    }
+
+    public void NextHole()
+    {
+        if (currentHole < course.getNumberOfHoles())
+        {
+            currentHole++;
+        }
+    }
+
+    public void PreviousHole()
+    {
+        if (currentHole > 1)
+        {
+            currentHole--;
+        }
     }
 
 
