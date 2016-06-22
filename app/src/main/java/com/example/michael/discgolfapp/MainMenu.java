59d7270183d3bf21bb98fa1160ef39c84e457b62
Michael Manley <Michael Manley>
2016-06-22 15:52:21 -0400

    Created a basic (still pretty) main menu. Have nothing wired up yet. I will be continuing on with creation of some of the other layouts and then I will move on to wiriing them all up.

---
+++ app/src/main/java/com/example/michael/discgolfapp/MainMenu.java
@@ -0,0 +1,18 @@
+package com.example.michael.discgolfapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Michael on 6/22/2016.
+ */
+public class MainMenu extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu);
+
+
+
+    }
+}
