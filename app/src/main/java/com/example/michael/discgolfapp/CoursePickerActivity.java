6a4b8602b80191ec9099c34ac431ac331357ad06
Michael Manley <Michael Manley>
2016-06-23 13:39:48 -0400

    Added a course picker layout and will populate it with similar style as the course editor layout

---
+++ app/src/main/java/com/example/michael/discgolfapp/CoursePickerActivity.java
@@ -0,0 +1,16 @@
+package com.example.michael.discgolfapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Michael on 6/23/2016.
+ */
+public class CoursePickerActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        
+    }
+}
