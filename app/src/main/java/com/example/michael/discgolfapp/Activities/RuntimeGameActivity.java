dd7b59ec996751bf248d06bbf77b37c8e2acca28
Michael Manley <Michael Manley>
2016-08-16 10:06:48 -0400

    Clean up the BS modified files

--- app/src/main/java/com/example/michael/discgolfapp/Activities/RuntimeGameActivity.java
+++ app/src/main/java/com/example/michael/discgolfapp/Activities/RuntimeGameActivity.java
@@ -129,7 +129,7 @@ public class RuntimeGameActivity extends AppCompatActivity implements IScrollVie
             titleCourseTextView.setText(course.getName());
             generateTables(players, course.getHoleCount());
 
-            
+
         }
 
         //If on recreation, pull out the saved state information and reset the object
