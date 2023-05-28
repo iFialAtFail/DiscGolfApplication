708da2b39182cf91dda38fe515f1684662bfec21
Michael Manley <mmanle01@gmail.com>
2023-05-28 16:17:39 -0400

    Upgrade Gradle/SDK Min, and most dependencies. Refactored Course.java to clean up inefficient code.

--- app/src/main/java/com/manleysoftware/michael/discgolfapp/data/Model/Course.java
+++ app/src/main/java/com/manleysoftware/michael/discgolfapp/data/Model/Course.java
@@ -1,6 +1,7 @@
 package com.manleysoftware.michael.discgolfapp.data.Model;
 
 import java.io.Serializable;
+import java.util.Arrays;
 
 /**
  * Created by Michael on 5/21/2016.
@@ -9,87 +10,56 @@ public class Course implements Serializable {
 
     public static final int MAX_HOLE_COUNT = 64;
 
-    //region private fields
     private int[] holes;
+
     private String courseName;
-    //endregion
 
-    //region Constructors
-
-    public Course(String courseName, int numHoles){
-
+    public Course(String courseName, int numHoles) {
         validateCourseName(courseName);
-        validateHoles(numHoles);
+        validateHoleCount(numHoles);
         initializeCoursePars(); //Set all holes to generic par 3.
     }
 
-    public Course (String courseName, int[] prePopulatedPars){
-
+    public Course(String courseName, int[] prePopulatedPars) {
         validateCourseName(courseName);
-
         validatePrePopulatedParValues(prePopulatedPars);
     }
 
-
-
-    //endregion
-
-    //region Getters and Setters
-
-    public String getName(){
+    public String getName() {
         return courseName;
     }
 
-    public void setName(String value){
+    public void setName(String value) {
         validateCourseName(value);
     }
 
-    public int getHoleCount(){
+    public int getHoleCount() {
         return holes.length;
     }
 
-    public int[] getParArray(){
+    public int[] getParArray() {
         return holes;
     }
 
-	public void setParArray(int[] parArray) {
-		this.holes = parArray;
-	}
-
-    public int getParTotal(){
-        int totalPars = 0;
-        for (int par : holes) {
-            totalPars += par;
-        }
-        return totalPars;
+    public void setParArray(int[] parArray) {
+        this.holes = parArray;
     }
 
+    public int getParTotal() {
+        return Arrays.stream(holes).sum();
+    }
 
-    //endregion
-
-    //region Private Helper Methods
-
-    private void initializeCoursePars()
-    {
-        for (int i = 0; i < holes.length; i++)
-        {
-            holes[i] = 3;
-        }
+    private void initializeCoursePars() {
+        Arrays.fill(holes, 3);
     }
 
     private boolean isValidParArray(int[] prePopulatedPars) {
-        for (int indexer : prePopulatedPars){
-            if (indexer <2){
-                return false;
-            }
-        }
-        return true;
+        return Arrays.stream(prePopulatedPars).noneMatch(i -> i < 2);
     }
 
-    public static boolean isValidCoursename(String nameInput)
-    {
+    public static boolean isValidCoursename(String nameInput) {
         nameInput = nameInput.trim();
-        if (nameInput.isEmpty()){
+        if (nameInput.isEmpty()) {
             return false;
         }
 
@@ -97,67 +67,48 @@ public class Course implements Serializable {
         //characters to ensure no special characters.
         char[] charArray = nameInput.toCharArray();
 
-        for(char letter : charArray)
-        {
-            if (Character.isLetterOrDigit(letter) || letter == ' ')
-            {
+        for (char letter : charArray) {
+            if (Character.isLetterOrDigit(letter) || letter == ' ') {
                 //Let it keep iterating through the characters.
-            }
-            else { return false; } //Break the cycle
+            } else {
+                return false;
+            } //Break the cycle
         }
         return true; //Continue if nothing was out of place.
     }
 
     private void validateCourseName(String courseName) {
-
-        if (isValidCoursename(courseName))
-        {
+        if (isValidCoursename(courseName)) {
             this.courseName = courseName;
-        }
-        else
-        {
+        } else {
             throw new IllegalArgumentException("Must be only AlphaNumeric characters.");
         }
     }
 
-    private void validateHoles(int numHoles) {
-
-        if (numHoles > 0) {
-            holes = new int[numHoles];
-        }
-
-        else {
+    private void validateHoleCount(int numHoles) {
+        if (numHoles <= 0) {
             throw new IllegalArgumentException("Holes must be greater than 0.");
         }
+        holes = new int[numHoles];
     }
 
     private void validatePrePopulatedParValues(int[] prePopulatedPars) {
-
-        if (prePopulatedPars.length > 0 && isValidParArray(prePopulatedPars)){
+        if (prePopulatedPars.length > 0 && isValidParArray(prePopulatedPars)) {
             holes = prePopulatedPars;
         }
     }
 
-    //endregion
-
-    //region Public Methods
-    public void IncrementHolePar(int currentHole)
-    {
+    public void IncrementHolePar(int currentHole) {
         holes[currentHole]++;
     }
 
-    public void DecrementHolePar(int currentHole)
-    {
+    public void DecrementHolePar(int currentHole) {
         if (holes[currentHole] > 1) {
 
             holes[currentHole]--;
         }
 
     }
-
-    //endregion
-
-    //region Overrides
 
     @Override
     public String toString() {
@@ -166,10 +117,7 @@ public class Course implements Serializable {
 
     @Override
     public boolean equals(Object o) {
-		return (o instanceof Course) && (((Course) o).getName().equals(this.getName()));
+        return (o instanceof Course) && (((Course) o).getName().equals(this.getName()));
     }
 
-
-
-	//endregion
 }
