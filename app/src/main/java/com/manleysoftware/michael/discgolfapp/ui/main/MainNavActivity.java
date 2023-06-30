83a1949de837dbd50a7c11e64fae8704f39b743b
Michael Manley <mmanle01@gmail.com>
2023-06-30 18:41:56 -0400

    Removed bottom nav bar, and replaced Fragment with FragmentContainerView

--- app/src/main/java/com/manleysoftware/michael/discgolfapp/ui/main/MainNavActivity.java
+++ app/src/main/java/com/manleysoftware/michael/discgolfapp/ui/main/MainNavActivity.java
@@ -3,11 +3,7 @@ package com.manleysoftware.michael.discgolfapp.ui.main;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.NavigationUI;
 
-import com.manleysoftware.michael.discgolfapp.R;
 import com.manleysoftware.michael.discgolfapp.databinding.ActivityMainNavBinding;
 
 import dagger.hilt.android.AndroidEntryPoint;
@@ -15,16 +11,12 @@ import dagger.hilt.android.AndroidEntryPoint;
 @AndroidEntryPoint
 public class MainNavActivity extends AppCompatActivity {
 
-    private ActivityMainNavBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityMainNavBinding.inflate(getLayoutInflater());
+        ActivityMainNavBinding binding = ActivityMainNavBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main_nav);
-        NavigationUI.setupWithNavController(binding.navView, navController);
     }
 
 }
